6ef041dddc852ed6337fde6a2ee86d785c61bd04
Muhammad Khan <mkhan@hub24.com.au>
2021-10-02 22:16:28 +1000

    chapter 8

---
+++ crash_course/chapter_8/functions.py
@@ -0,0 +1,13 @@
+#task8-1
+def display_message():
+    print("You are learning about python functions")
+
+display_message()
+
+#task8-2
+print("\n")
+
+def favorite_book(book_title):
+    print(f"One of my favorite book is '{book_title.title()}'.")
+
+favorite_book("Alice in wounderland")
---
+++ crash_course/chapter_8/importng.py
@@ -0,0 +1,8 @@
+import functions
+import functions as func
+
+from functions import display_message
+from functions import display_message as dm
+
+from functions import display_message, favorite_book
+
